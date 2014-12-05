c77f8d4ec82a1a2eabc2e024fac15f65aece3699
Darkwing1012 <dylan8522@gmail.com>
2014-12-05 13:01:56 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,6 +105,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://rawgit.com/Darkwing1012/DarkwingBot/master/DarkwingBot.js', extend);
 
 }).call(this);
